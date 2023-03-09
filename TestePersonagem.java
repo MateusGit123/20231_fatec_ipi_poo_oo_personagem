f0bc50c5949d9b461f0fd6202bde44d741b1f2b0
Aluno <aluno@IPIRANGA.FATEC>
2023-03-09 15:57:30 -0300

    apostila02(oo): bloco de codigo 2.2.1 encerrado

---
+++ TestePersonagem.java
@@ -0,0 +1,9 @@
+public class TestePersonagem {
+    static public void main(String [] args){
+        var p = new Personagem();
+        p.nome = "John";
+        p.cacar();
+        p.comer();
+        p.dormir();
+    }
+}
