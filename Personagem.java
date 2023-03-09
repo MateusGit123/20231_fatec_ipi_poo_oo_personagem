47509e70ea0c0dab585f48427df4f6a39b9eb076
Aluno <aluno@IPIRANGA.FATEC>
2023-03-09 17:54:27 -0300

    apostila02(oo): bloco de codigo 2.5.1 encerrado

--- Personagem.java
+++ Personagem.java
@@ -6,40 +6,58 @@ public class Personagem{
     int fome = 0;
     int sono = 0;
 
+    //Construtor padrão: lista de parâmetros vazia
+    Personagem(int energia, int fome, int sono){
+        //shadowing(sombreamento)
+        if (energia >= 0 && energia <= 10)
+        this.energia = energia;
+        if (fome >= 0 && fome <= 10)
+        this.fome = fome;
+        if (sono >= 0 && sono <= 10)
+        this.sono = sono;
+        System.out.println("Construindo um objeto...");
+    }
+
     //isso é metodo de instância
     void cacar(){
         if (energia >= 2){
-        System.out.println(nome + "cacando");
+        System.out.println(nome +  "cacando");
         energia = energia - 2; //Ou voce pode escrever energia -= 2
     }
     else{
-        System.out.println(nome + "sem energia para cacar");
+        System.out.println(nome +  "sem energia para cacar");
         fome = min(fome + 1, 10);
-        sono = min(fome + 1, 10);
+        sono = min(sono + 1, 10);
     }
+    System.out.println(obterEstado());
 }
 
 
     void comer(){
         if (fome >= 1){
-        System.out.println(nome + "comendo");
+        System.out.println(nome +  "comendo");
         energia = min (energia +1, 10);
         fome -= 1;
     }
     else{
-        System.out.println(nome + "sem fome");
+        System.out.println(nome +  "sem fome");
     }
+    System.out.println(obterEstado());
 }
 
 
     void dormir(){
         if (sono >= 1){
-        System.out.println(nome + "dormindo");
+        System.out.println(nome +  "dormindo");
         energia = energia + 1 < 10 ? energia + 1 : 10; //O : é o "se nao"
         sono --;
     }
     else{
-        System.out.println(nome + "sem sono");
+        System.out.println(nome +  "sem sono");
     }
-}
+    System.out.println(obterEstado());
+    }
+    String obterEstado(){
+        return String.format("e(%d), s(%d), f(%d)", energia, sono, fome);
+    }
 }
--- TestePersonagem.java
+++ TestePersonagem.java
@@ -1,9 +1,27 @@
 public class TestePersonagem {
-    static public void main(String [] args){
-        var p = new Personagem();
-        p.nome = "John";
-        p.cacar();
-        p.comer();
-        p.dormir();
+    static public void main(String [] args) throws Exception{
+        //p é uma variavel de referencia. Ela faz referencia ao objeto construido com new
+        //Personagem p = new Personagem();
+        //java 10+ (inferência de tipo)
+        var p = new Personagem(2, 4, 10);
+        var p2 = new Personagem(8, 10, 2);
+        p.nome = "John ";
+       
+        while (true){
+            p.cacar();
+            Thread.sleep(5000);
+            p.comer();
+            Thread.sleep(5000);
+            p.dormir();
+            Thread.sleep(5000);
+            p.cacar();
+            Thread.sleep(5000);
+            p.cacar();
+            Thread.sleep(5000);
+            p.cacar();
+            Thread.sleep(5000);
+            System.out.println("=====================================");
+            Thread.sleep(10000);
+        }
     }
 }
